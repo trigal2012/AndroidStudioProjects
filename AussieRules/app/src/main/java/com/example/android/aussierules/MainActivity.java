697676791efe3073844c01651d82b1f9634aed65
trigal2012 <keeplist1@gmail.com>
2018-02-10 01:00:21 -0800

    AussieRules - updates to xml and java code

--- AussieRules/app/src/main/java/com/example/android/aussierules/MainActivity.java
+++ AussieRules/app/src/main/java/com/example/android/aussierules/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.android.aussierules;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.ArrayMap;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -35,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         resetData();
         updateEverything();
+        removeBallIcon(R.id.ballIconTeam1);
+        removeBallIcon(R.id.ballIconTeam2);
+        disablePlayButton();
     }
 
     /* method used to update display with integer values */
@@ -48,6 +53,30 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(msg);
     }
 
+    private void setBallIcon(Integer teamId){
+        ImageView ballIcon = (ImageView)findViewById(teamId);
+        ballIcon.setVisibility(View.VISIBLE);
+    }
+
+    private void removeBallIcon(Integer teamId){
+        ImageView ballIcon = (ImageView)findViewById(teamId);
+        ballIcon.setVisibility(View.INVISIBLE);
+    }
+
+    private void disablePlayButton(){
+        Button btn = (Button) findViewById(R.id.playButton);
+        btn.setEnabled(false);
+    }
+
+    private void enablePlayButton(){
+        Button btn = (Button) findViewById(R.id.playButton);
+        btn.setEnabled(true);
+    }
+
+    private void enableNewGameButton(){
+        Button btn = (Button)findViewById(R.id.reset);
+        btn.setEnabled(true);
+    }
     private void resetData() {
         /* data for game play */
         numOfPlays = 0;
@@ -69,39 +98,61 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateEverything() {
         /* update team1 stats */
-        scoreTeam1 = rBehindsTeam2 + behindsTeam1 + goalsTeam1 * 6;
+        int behinds1 = rBehindsTeam2 + behindsTeam1;
+        scoreTeam1 = behinds1 + goalsTeam1 * 6;
         updateDisplay(R.id.scoreTeam1, scoreTeam1);
-        updateDisplay(R.id.numOfTryTeam1, numOfTryTeam1);
         updateDisplay(R.id.goalsTeam1, goalsTeam1);
         updateDisplay(R.id.behindsTeam1, behindsTeam1);
         updateDisplay(R.id.rBehindsTeam1, rBehindsTeam1);
 
         /* update team2 stats */
-        scoreTeam2 = rBehindsTeam1 + behindsTeam2 + goalsTeam2 * 6;
+        int behinds2 = rBehindsTeam1 + behindsTeam2;
+        scoreTeam2 = behinds2 + goalsTeam2 * 6;
         updateDisplay(R.id.scoreTeam2, scoreTeam2);
-        updateDisplay(R.id.numOfTryTeam2, numOfTryTeam2);
         updateDisplay(R.id.goalsTeam2, goalsTeam2);
         updateDisplay(R.id.behindsTeam2, behindsTeam2);
         updateDisplay(R.id.rBehindsTeam2, rBehindsTeam2);
 
         /* disable play button when there are no Trys left */
         if(numOfPlays == 10 ) {
-            Button btn = (Button) findViewById(R.id.playButton);
-            btn.setEnabled(false);
+            disablePlayButton();
 
             /* update score display to indicate game is over */
-            String team1 = goalsTeam1 + "." + behindsTeam1 + "(" + scoreTeam1 + ")";
-            String team2 = goalsTeam2 + "." + behindsTeam2 + "(" + scoreTeam2 + ")";
+            String team1 = goalsTeam1 + "." + behinds1 + "(" + scoreTeam1 + ")";
+            String team2 = goalsTeam2 + "." + behinds2 + "(" + scoreTeam2 + ")";
             updateDisplayString(R.id.scoreTeam1, team1);
             updateDisplayString(R.id.scoreTeam2, team2);
+            setBallIcon(R.id.ballIconTeam1);
+            setBallIcon(R.id.ballIconTeam2);
+
+            if(scoreTeam1 > scoreTeam2){
+                TextView scoreBold1 = (TextView)findViewById(R.id.scoreTeam1);
+                scoreBold1.setTypeface(null, Typeface.BOLD);
+                TextView scoreBold2 = (TextView)findViewById(R.id.scoreTeam2);
+                scoreBold2.setTypeface(null, Typeface.NORMAL);
+            }else if(scoreTeam2 > scoreTeam1){
+                TextView scoreBold1 = (TextView)findViewById(R.id.scoreTeam1);
+                scoreBold1.setTypeface(null, Typeface.NORMAL);
+                TextView scoreBold2 = (TextView)findViewById(R.id.scoreTeam2);
+                scoreBold2.setTypeface(null, Typeface.BOLD);
+            }else {
+                TextView scoreBold1 = (TextView)findViewById(R.id.scoreTeam1);
+                scoreBold1.setTypeface(null, Typeface.BOLD);
+                TextView scoreBold2 = (TextView)findViewById(R.id.scoreTeam2);
+                scoreBold2.setTypeface(null, Typeface.BOLD);
+            }
         }
     }
 
     public void newGame(View v) {
         resetData();
         updateEverything();
-        Button btn = (Button) findViewById(R.id.playButton);
-        btn.setEnabled(true);
+        enablePlayButton();
+        setBallIcon(R.id.ballIconTeam1);
+        removeBallIcon(R.id.ballIconTeam2);
+        updateDisplay(R.id.numOfTries, numOfTryTeam1);
+        TextView trySize = (TextView)findViewById(R.id.numOfTries);
+        trySize.setTextSize(100);
     }
 
     public void runPlay(View v) {
@@ -111,7 +162,7 @@ public class MainActivity extends AppCompatActivity {
 
         numOfPlays = numOfPlays + 1;
 
-        if (numOfPlays >= 1 && numOfPlays < 6) {
+        if (numOfPlays >= 1 && numOfPlays <= 5) {
             if (playResult == 1) {
                 rBehindsTeam1 = rBehindsTeam1 + 1;
             }
@@ -123,9 +174,18 @@ public class MainActivity extends AppCompatActivity {
             }
             numOfTryTeam1 = numOfTryTeam1 - 1;
             updateEverything();
+            updateDisplay(R.id.numOfTries, numOfTryTeam1);
+
+            if(numOfTryTeam1 == 0){
+                removeBallIcon(R.id.ballIconTeam1);
+                setBallIcon(R.id.ballIconTeam2);
+                updateDisplay(R.id.numOfTries, numOfTryTeam2);
+            }
         }
 
         if (numOfPlays >= 6 && numOfPlays <= 10) {
+         removeBallIcon(R.id.ballIconTeam1);
+         setBallIcon(R.id.ballIconTeam2);
             if (playResult == 1) {
                 rBehindsTeam2 = rBehindsTeam2 + 1;
             }
@@ -137,6 +197,13 @@ public class MainActivity extends AppCompatActivity {
             }
             numOfTryTeam2 = numOfTryTeam2 - 1;
             updateEverything();
+            updateDisplay(R.id.numOfTries, numOfTryTeam2);
+
+            if(numOfTryTeam2 == 0){
+                updateDisplayString(R.id.numOfTries, "GameOver");
+                TextView gameOver = (TextView)findViewById(R.id.numOfTries);
+                gameOver.setTextSize(25);
+            }
         }
 
     }
