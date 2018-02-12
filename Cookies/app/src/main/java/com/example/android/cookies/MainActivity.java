e961aec05a4ff125fc2648aaf99936194218f91b
trigal2012 <keeplist1@gmail.com>
2018-02-11 19:37:22 -0800

    lesson 10 Cookies

---
+++ Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.cookies;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the cookie should be eaten.
+     */
+    public void eatCookie(View view) {
+        ImageView changeImage = findViewById(R.id.android_cookie_image_view);
+        changeImage.setImageResource(R.drawable.after_cookie);
+
+        TextView changeText = findViewById(R.id.status_text_view);
+        changeText.setText("I'm so full");
+    }
+}
