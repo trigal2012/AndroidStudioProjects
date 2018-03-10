e8702aaac97d6e3a9773c20f028fc60678b4a570
Diana garbarino <diana@versame.com>
2018-03-09 18:57:15 -0800

    mindsport refactor styles and strings

--- MindSport/app/src/main/java/com/example/android/mindsport/MainActivity.java
+++ MindSport/app/src/main/java/com/example/android/mindsport/MainActivity.java
@@ -1,30 +1,55 @@
 package com.example.android.mindsport;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
-import android.widget.Toast;
 import android.view.View;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.question3);
+        setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.submit);
+        //setting up buttons to call the onClick method when app loads
+        Button start = findViewById(R.id.start);
+        start.setOnClickListener(this);
+        Button next1 = findViewById(R.id.next1);
+        next1.setOnClickListener(this);
+        Button next2 = findViewById(R.id.next2);
+        next2.setOnClickListener(this);
+        Button submit = findViewById(R.id.submit);
+        submit.setOnClickListener(this);
 
-        // Show toast message when button is clicked
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast toast = Toast.makeText(getApplicationContext(), "Simple Toast In Android", Toast.LENGTH_LONG); // initiate the Toast with context, message and duration for the Toast
-                toast.setGravity(android.view.Gravity.TOP | android.view.Gravity.LEFT, 0, 0);     // set gravity for the Toast.
-                toast.show(); // display the Toast
-            }
-        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.start:
+                Log.i("tag", "start button pressed");
+                break;
+
+            case R.id.next1:
+                Log.i("tag", "next1 button pressed");
+                break;
+
+            case R.id.next2:
+                Log.i("tag", "next2 button pressed");
+                break;
+
+            case R.id.submit:
+                Log.i("tag", "submit button pressed");
+                break;
+
+            default:
+                break;
+        }
 
     }
 }
