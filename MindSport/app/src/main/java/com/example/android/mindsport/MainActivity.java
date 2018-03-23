2f8cedd25830f9818b50784b2f1905ec6e706576
Diana garbarino <diana@versame.com>
2018-03-22 17:33:41 -0700

    refactor to get the quiz project to run a little better

--- MindSport/app/src/main/java/com/example/android/mindsport/MainActivity.java
+++ MindSport/app/src/main/java/com/example/android/mindsport/MainActivity.java
@@ -1,257 +1,219 @@
 package com.example.android.mindsport;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Button;
 import android.view.View;
-import android.view.LayoutInflater;
-import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.Button;
+import android.util.Log;
 import android.widget.TextView;
-
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
 import android.widget.Toast;
+import android.view.LayoutInflater;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    //Game Data
+    //initialize vars
     String playerName = "";
     String a1 = "";
     String a2 = "";
-    String a4 = "";
-    String a5 = "";
-    String a6_1 = "f";
-    String a6_2 = "f";
-    String a6_3 = "f";
     String a6 = "";
-    String a7_1 = "f";
-    String a7_2 = "f";
-    String a7_3 = "f";
     String a7 = "";
     String toastMessage = "";
-    int score = 0;
+
     int layoutId = R.layout.activity_main;
+    int score = 0;
+
+    Boolean a4 = false;
+    Boolean a5 = false;
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
+        if(layoutId == R.layout.question1){
+            a1 = getEditTextAnswers(R.id.a1);
+            a2 = getEditTextAnswers(R.id.a2);
+        }
+        if(layoutId == R.layout.question2){
+            getRBAnswers(R.id.rg4);
+            getRBAnswers(R.id.rg5);
+        }
+        if(layoutId == R.layout.question3){
+            getCBAnswers();
+            determineScore();
+        }
+        outState.putInt("layoutId", layoutId);
         outState.putString("playerName", playerName);
         outState.putString("a1", a1);
         outState.putString("a2", a2);
-        outState.putString("a4", a4);
-        outState.putString("a5", a5);
+        outState.putBoolean("a4", a4);
+        outState.putBoolean("a5", a5);
         outState.putString("a6", a6);
         outState.putString("a7", a7);
         outState.putInt("score", score);
-        outState.putInt("layoutId", layoutId);
-
         super.onSaveInstanceState(outState);
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        // Always call the superclass so it can restore the view hierarchy
-        super.onRestoreInstanceState(savedInstanceState);
-        // Restore state members from saved instance
-        if (savedInstanceState != null) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null && (savedInstanceState.getInt("layoutId") != R.layout.activity_main)) {
+            Log.i("tag", "on create if layoutid is: " + layoutId);
+            layoutId = savedInstanceState.getInt("layoutId");
             playerName = savedInstanceState.getString("playerName");
             a1 = savedInstanceState.getString("a1");
             a2 = savedInstanceState.getString("a2");
-            a4 = savedInstanceState.getString("a4");
-            a5 = savedInstanceState.getString("a5");
+            a4 = savedInstanceState.getBoolean("a4");
+            a5 = savedInstanceState.getBoolean("a5");
             a6 = savedInstanceState.getString("a6");
             a7 = savedInstanceState.getString("a7");
             score = savedInstanceState.getInt("score");
-            layoutId = savedInstanceState.getInt("layoutId");
-            setContentView(layoutId);
-
         }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(layoutId);
 
-        //setting up buttons to call the onClick method when app loads
-        Button start = findViewById(R.id.start);
-        start.setOnClickListener(this);
-
     }
 
-
-    public void onCheckboxClicked(View view) {
-        // Is the view now checked?
-        boolean checked = ((CheckBox) view).isChecked();
-
-        // Check which checkbox was clicked
+    public void onButtonClick(View view){
         switch (view.getId()) {
-            case R.id.a6_1:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a6_1");
-                    a6_1 = "t";
-                } else
-                    a6_1 = "f";
-                break;
-            case R.id.a6_2:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a6_2");
-                    a6_2 = "t";
-                } else
-                    a6_2 = "f";
-                break;
-            case R.id.a6_3:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a6_3");
-                    a6_3 = "t";
-                } else
-                    a6_3 = "f";
-                break;
-            case R.id.a7_1:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a7_1");
-                    a7_1 = "t";
-                } else
-                    a7_1 = "f";
-                break;
-            case R.id.a7_2:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a7_2");
-                    a7_2 = "t";
-                } else
-                    a7_2 = "f";
-                break;
-            case R.id.a7_3:
-                if (checked) {
-                    Log.d("tag", "onCheckboxClicked: a7_3");
-                    a7_3 = "t";
-                } else
-                    a7_3 = "f";
-                break;
-        }
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        switch (v.getId()) {
-
-            //Button listeners
             case R.id.start:
                 getPlayerName();
-                layoutId = R.layout.question1;
-                setContentView(layoutId);
-                updateDisplay(R.id.gameName, playerName);
+                changeLayout(R.layout.question1, getString(R.string.title));
                 break;
-
             case R.id.next1:
-                a1 = getAnswer(a1, R.id.a1);
-                Log.i("tag", a1);
-                a2 = getAnswer(a2, R.id.a2);
-                Log.i("tag", a2);
-                layoutId = (R.layout.question2);
-                setContentView(layoutId);
-                updateDisplay(R.id.gameName, playerName);
+                a1 = getEditTextAnswers(R.id.a1);
+                a2 = getEditTextAnswers(R.id.a2);
+                changeLayout(R.layout.question2, getString(R.string.title));
                 break;
-
             case R.id.next2:
-                Log.i("tag", a4);
-                Log.i("tag", a5);
-                layoutId=(R.layout.question3);
-                setContentView(layoutId);
-                updateDisplay(R.id.gameName, playerName);
+                getRBAnswers(R.id.rg4);
+                getRBAnswers(R.id.rg5);
+                changeLayout(R.layout.question3, getString(R.string.title));
                 break;
-
             case R.id.submit:
-                a6 = a6_1 + a6_2 + a6_3;
-                a7 = a7_1 + a7_2 + a7_3;
-                Log.i("tag", a6);
-                Log.i("tag", a7);
-                toastMessage = "";
+                getCBAnswers();
                 determineScore();
+                changeLayout(R.layout.game_over, getString(R.string.gameOver));
                 createMessage();
                 displayToast(toastMessage);
-                TextView title = (TextView)findViewById(R.id.gameName);
-                title.setText("GAME OVER");
-                Button btn = (Button)findViewById(R.id.submit);
-                btn.setEnabled(false);
-                btn.setBackgroundColor(getResources().getColor(R.color.darkGray));
-                btn.setTextColor(getResources().getColor(R.color.lightGray));
-                disableCheckbox(R.id.a6_1);
-                disableCheckbox(R.id.a6_2);
-                disableCheckbox(R.id.a6_3);
-                disableCheckbox(R.id.a7_1);
-                disableCheckbox(R.id.a7_2);
-                disableCheckbox(R.id.a7_3);
                 break;
-
-            default:
+            case R.id.playAgain:
+                resetVars();
+                layoutId = R.layout.activity_main;
+                setContentView(layoutId);
                 break;
         }
 
+    }
+
+    private void resetVars() {
+        playerName = "";
+        a1 = "";
+        a2 = "";
+        a6 = "";
+        a7 = "";
+        toastMessage = "";
+
+        layoutId = R.layout.activity_main;
+        score = 0;
+
+        a4 = false;
+        a5 = false;
+    }
+
+    private void changeLayout(int layout, String newText){
+        layoutId = layout;
+        setContentView(layoutId);
+        TextView name = (TextView)findViewById(R.id.gameName);
+        name.setText(newText);
+    }
+
+    private void getPlayerName(){
+        EditText tempName = (EditText)findViewById(R.id.pname);
+        playerName = tempName.getText().toString();
+    }
+
+    private String getEditTextAnswers(int answerId){
+        EditText tempAns = (EditText)findViewById(answerId);
+        return tempAns.getText().toString().toLowerCase();
+    }
+
+    private void getRBAnswers(int radioGroupId){
+        RadioGroup radioGroup = (RadioGroup) findViewById(radioGroupId);
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+
+        if(radioGroupId == R.id.rg4) {
+            // is the correct one selected?
+            if (selectedId == R.id.a4_3) {
+                a4 = true;
+            }
+        }
+        if(radioGroupId == R.id.rg5){
+            // is the correct one selected?
+            if (selectedId == R.id.a5_3) {
+                a5 = true;
+            }
+        }
+    }
+
+    private void getCBAnswers(){
+        a6 = "";
+        a7 = "";
+        CheckBox cbAns = (CheckBox)findViewById(R.id.a6_1);
+        if(cbAns.isChecked()) {
+            a6 += "t";
+        }
+        cbAns = (CheckBox)findViewById(R.id.a6_2);
+        if(cbAns.isChecked()){
+            a6 += "t";
+        }
+        cbAns = (CheckBox)findViewById(R.id.a6_2);
+        if(cbAns.isChecked()) {
+            a6 += "t";
+        }
+        cbAns = (CheckBox)findViewById(R.id.a7_1);
+        if(cbAns.isChecked()) {
+            a7 += "t";
+        }
+        cbAns = (CheckBox)findViewById(R.id.a7_2);
+        if(cbAns.isChecked()) {
+            a7 += "t";
+        }
+        cbAns = (CheckBox)findViewById(R.id.a7_3);
+        if(cbAns.isChecked()) {
+            a7 += "t";
+        }
     }
 
     private void determineScore(){
-        if (a1.toLowerCase().equals("macbeth")){
+        Log.i("tag", "a1: " + a1);
+        Log.i("tag", "a2: " + a2);
+        Log.i("tag", "a4: " + a4);
+        Log.i("tag", "a5: " + a5);
+        Log.i("tag", "a6: " + a6);
+        Log.i("tag", "a7: " + a7);
+        if (a1.equals("macbeth")){
             score += 1;
         }
-        if (a2.toLowerCase().equals("i")){
+        if (a2.equals("i")){
             score += 1;
         }
-        if(a4.toLowerCase().equals("a4_3")){
+        if(a4 == true){
             score += 1;
         }
-        if(a5.toLowerCase().equals("a5_3")){
+        if(a5 == true){
             score += 1;
         }
         if(a6.equals("ttt")){
             score += 1;
         }
-        if(a7.equals("tft"))
+        if(a7.equals("t")) {
             score += 1;
-    }
-
-    private void getPlayerName(){
-        EditText tempPlayerName = (EditText) findViewById(R.id.pname);
-        playerName = tempPlayerName.getText().toString();
-        Log.i("tag", "start button pressed. player name is: " + playerName);
-    }
-
-    private String getAnswer(String answerNum, int answerId){
-        EditText tempAnswer = (EditText) findViewById(answerId);
-        answerNum = tempAnswer.getText().toString();
-        return answerNum;
-    }
-
-    private void updateDisplay(int viewID, String textValue){
-        TextView tempView = (TextView) findViewById(viewID);
-        tempView.setText(String.valueOf(textValue) );
-    }
-
-    private void updateLayout(int currentLayout, int newLayout){
-        //remove views no longer needed
-        LinearLayout removeMe = (LinearLayout) findViewById(currentLayout);
-        removeMe.removeAllViewsInLayout();
-
-        //find the questions section
-        LinearLayout questionsView = (LinearLayout) findViewById(currentLayout);
-
-        //add new questions to the question section
-        LinearLayout questions = (LinearLayout) View.inflate(this, newLayout, null);
-        questionsView.addView(questions);
-    }
-
-    private void updateButton(String newText, int currentId, int newId){
-        //change text and id on button
-        Button newBtnValue = (Button)findViewById(currentId);
-        newBtnValue.setText(newText);
-        newBtnValue.setId(newId);
+        }
+        Log.i("tag", "score: " + score);
     }
 
     private void createMessage(){
+        toastMessage = "";
         toastMessage += getString(R.string.salutation) + " " + playerName + ",\n";
         toastMessage += getString(R.string.thanks) + "\n";
         toastMessage += getString(R.string.youHave) + " " + score + " of 6 " + getString(R.string.correct) + " " + getString(R.string.answer);
@@ -275,9 +237,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         toast.show();
     }
 
-    private void disableCheckbox(int chkBoxId){
-        CheckBox checkBox = (CheckBox)findViewById(chkBoxId);
+    private void disableCheckbox(int checkboxId){
+        CheckBox checkBox = (CheckBox)findViewById(checkboxId);
         checkBox.setEnabled(false);
+    }
+
+    private void disableAllCheckboxes(){
+        disableCheckbox(R.id.a6_1);
+        disableCheckbox(R.id.a6_2);
+        disableCheckbox(R.id.a6_3);
+        disableCheckbox(R.id.a7_1);
+        disableCheckbox(R.id.a7_2);
+        disableCheckbox(R.id.a7_3);
+    }
+
+    private void disableSubmitBtn(){
+        Button btn = (Button)findViewById(R.id.submit);
+        btn.setEnabled(false);
+        btn.setBackgroundColor(getResources().getColor(R.color.darkGray));
+        btn.setTextColor(getResources().getColor(R.color.lightGray));
     }
 
 }
--- MyApplication/app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -3,29 +3,35 @@ package com.example.android.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.content.Intent;
 import android.widget.TextView;
 import android.util.Log;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     String edit1 = "";
     String playerName = edit1;
     int activeLayout = R.layout.activity_main;
     int actvity_main = R.layout.activity_main;
-    int page1 = R.layout.message_page1;
-    int page2 = R.layout.message_page2;
-
+    boolean cb2State = false;
+    int cb1 = R.id.cb1;
+    int cb2 = R.id.cb2;
+    int cb3 = R.id.cb3;
 
     @Override
     protected void onSaveInstanceState(Bundle state) {
         super.onSaveInstanceState(state);
         state.putInt("activeLayout", activeLayout);
         state.putString("edit1", edit1);
-        state.putString("playerName", edit1);
+        state.putBoolean("cb2State",cb2State );
         Log.i("tag", "onSaveInstance active layout is: " + activeLayout);
     }
-    
+
 
     @Override
     protected void onCreate(Bundle bundle) {
@@ -36,25 +42,31 @@ public class MainActivity extends AppCompatActivity {
             activeLayout = bundle.getInt("activeLayout");
             edit1 = bundle.getString("edit1");
             playerName = bundle.getString("playerName");
+            cb2State = bundle.getBoolean("cb2State");
         }
         setContentView(activeLayout);
         if(activeLayout != actvity_main){
             Log.i("tag", "not active_main");
             TextView playerName = (TextView)findViewById(R.id.textView2);
             playerName.setText(edit1);
+            CheckBox cb2S = (CheckBox)findViewById(R.id.cb2);
+            cb2S.setChecked(cb2State);
         }else{
             EditText playerName = (EditText)findViewById(R.id.edit1);
             playerName.setText(edit1);
         }
+        Log.i("tag", ""+cb2State);
     }
 
     public void page1(View view){
         EditText edit = (EditText)findViewById(R.id.edit1);
         edit1 = edit.getText().toString();
+        CheckBox cb2S = (CheckBox)findViewById(R.id.cb2);
+        cb2State = cb2S.isChecked();
         setContentView(R.layout.message_page1);
         TextView playerName = (TextView)findViewById(R.id.textView2);
         playerName.setText(edit1);
-        activeLayout = R.layout.message_page1;
+
         Log.i("tag", "page1 button active layout is: " + activeLayout);
     }
 
@@ -62,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.message_page2);
         TextView playerName = (TextView)findViewById(R.id.textView2);
         playerName.setText(edit1);
+        Log.i("tag", "the value of cb2 is: " + cb2State);
         activeLayout = R.layout.message_page2;
         Log.i("tag", "page2 button active layout is: " + activeLayout);
     }
